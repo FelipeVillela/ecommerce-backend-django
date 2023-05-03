96cf0c8c8d3fb22d3db0d1212e1ad03732045611
Felipe Villela <felipeavillela@gmail.com>
2023-05-03 20:46:38 -0300

    Incluido tabela users aos models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
+class Users(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    email = models.EmailField('Email', unique=True)
+    password = models.CharField('password', max_length=255, null=True)
+    birth_date = models.DateField('Data de nascimento')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+    def save(self, *args, **kwargs):
+        self.password = make_password(self.password, hasher='md5')
+        super().save(*args, **kwargs)
+
