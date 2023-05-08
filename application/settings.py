ab10008b3c907850923d142ec4504f217d3b5e48
Felipe Villela <felipeavillela@gmail.com>
2023-05-08 16:01:35 -0300

    Settings env + language code

--- application/settings.py
+++ application/settings.py
@@ -16,11 +16,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Vairáveis de ambiente
+
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
+
+APP_JWT_SECRET=os.getenv('APP_JWT_SECRET')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -146,8 +150,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# Vairáveis de ambiente
-
-APP_JWT_SECRET='B!K<wGGnk&Lt!I9O'
 
