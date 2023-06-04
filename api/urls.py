92565a9f4301634387700f504766cb96914b34e7
Felipe Villela <felipeavillela@gmail.com>
2023-06-04 20:25:29 -0300

    Some changes

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,10 @@ from django.urls import include, path
 from rest_framework import routers
 from .views import UsersViewSet
 
+app_name = 'api'
+
 router = routers.DefaultRouter()
-router.register(r'users', UsersViewSet)
+router.register(r'users', UsersViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls))
